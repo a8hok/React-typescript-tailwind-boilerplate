9f34bcec77238e4d31db1e454292933c9ffde753
a8hok <ashokkumar.explore@gmail.com>
2023-05-30 10:16:27 +0530

    added components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
+import Home from './Pages/Home';
+
+const App: React.FC = () => {
   return (
-    <>
-      <p className="bg-TK-background">
-        React Typescript and Tailwind Kick starter
-      </p>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
