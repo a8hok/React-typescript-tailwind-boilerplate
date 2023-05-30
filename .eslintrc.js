0661699d78ad94a0fc811c04f752ffde0cd46fd1
a8hok <ashokkumar.explore@gmail.com>
2023-05-30 06:40:51 +0530

    added eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/strict',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    "no-use-before-define": "error",
+    "no-unused-vars": "error",
+    "indent": ["error", 2]
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
