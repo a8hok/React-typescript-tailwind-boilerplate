710052d116e4d96a8362d8879d5a71610407c1af
a8hok <ashokkumar.explore@gmail.com>
2023-05-30 06:50:39 +0530

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: ['./tsconfig.json'],
     ecmaVersion: 2021,
     sourceType: 'module',
     ecmaFeatures: {
