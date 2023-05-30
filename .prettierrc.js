96d5efa0e9d05c71ea1ce9967b084c61d1d9a17c
a8hok <ashokkumar.explore@gmail.com>
2023-05-30 06:51:13 +0530

    updated prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  useTabs: false,
+}
